c6efca8e1b28ca40b949239a3e2f78879b8bd03f
raquel <raquelramoscampos285@gmail.com>
2025-05-15 12:22:23 -0300

    soma e num impar e par

--- exercício1/script.js
+++ exercício1/script.js
@@ -1,10 +1,29 @@
 let num1;
 let num2;
 let resultado; 
+let parouimpar
 
 num1 = Number(window.prompt("Digite aqui o primeiro número:"));
 num2 = Number(window.prompt("Digite o segundo número:"));
 
 resultado = num1 + num2;
 
-alert(resultado);
+parouimpar = resultado % 2;
+
+if(parouimpar == 0) {
+    alert(" O número " + resultado + " é par ")
+} else {
+    alert(" O número " + resultado + " é ímpar ")
+}
+
+if(resultado > 10) {
+    alert(" o número " + resultado + " é maior do que 10 ");
+} else {
+    alert(" o número " + resultado + " é menor ou igual a que 10 ");
+}
+
+//DESAFIO: Informar também, na mesma frase, se o número é par ou ímpar.
+
+/*if(resultado > 10) {
+    alert("o número " + resultado + "é maior do que 10");
+ } */
