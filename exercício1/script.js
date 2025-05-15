213f0938a5caa1c2d214b81e5ca182078a1caed3
raquel <raquelramoscampos285@gmail.com>
2025-05-15 10:01:15 -0300

    .

---
+++ exercício1/script.js
@@ -0,0 +1,10 @@
+let num1;
+let num2;
+let resultado; 
+
+num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+num2 = Number(window.prompt("Digite o segundo número:"));
+
+resultado = num1 + num2;
+
+alert(resultado);
