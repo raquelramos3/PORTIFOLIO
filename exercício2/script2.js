0cd11206ff7c6c6040c963c222590245ef7920fa
raquel <raquelramoscampos285@gmail.com>
2025-05-22 09:40:08 -0300

    operações mat

---
+++ exercício2/script2.js
@@ -0,0 +1,40 @@
+function somar(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+    let num2 = Number(window.prompt("Digite o segundo número:"));
+
+    let resultado = num1 + num2;
+
+    alert("O resultado é " + resultado + ".");
+}
+
+function subtrair(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+    let num2 = Number(window.prompt("Digite o segundo número:"));
+
+    let resultado = num1 - num2;
+
+    alert("O resultado é " + resultado + ".");
+}
+
+function dividir(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+    let num2 = Number(window.prompt("Digite o segundo número:"));
+
+    let resultado = num1/num2;
+
+    alert("O resultado é " + resultado + ".");
+}
+
+function multiplicar(){
+    let num1 = Number(window.prompt("Digite aqui o primeiro número:"));
+    let num2 = Number(window.prompt("Digite o segundo número:"));
+
+    let resultado = num1 * num2;
+
+    alert("O resultado é " + resultado + ".");
+}
+
+
+
+
+
