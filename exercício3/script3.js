3e9408672cd3df0fdecfddd677f5150bb5e8eb9b
raquel <raquelramoscampos285@gmail.com>
2025-05-27 12:24:12 -0300

    CALCULADORA/MÉDIA

--- exercício3/script3.js
+++ exercício3/script3.js
@@ -1,16 +1,21 @@
 function somar(){
-    let nota1 =  Number(window.prompt("Digite aqui sua primeira nota:"));
-    let nota2 =  Number(window.prompt("Digite aqui sua segunda nota:"));
-    let nota3 =  Number(window.prompt("Digite aqui sua terceira nota:"));
+    let nome
+    let nota1
+    let nota2
+    let nota3
+
+     nome = window.prompt("Digite aqui seu nome: ");
+     nota1 = Number(window.prompt(" Olá " + nome + "!" + "Digite aqui sua primeira nota:"));
+     nota2 = Number(window.prompt("Digite aqui sua segunda nota:"));
+     nota3 = Number(window.prompt("Digite aqui sua terceira nota:"));
 
     let resultado = nota1 + nota2 + nota3 /3
     
-   if (resultado => 180){
-        alert(alert("Você passou!" + resultado + "."););      
-   } else {
-        (if resultado < 180) 
-            alert(Você não passou!) }
-
+    if(resultado < 60) {
+     alert(" Não passou! " + resultado + " é menor que 60! ");
+ } else {
+     alert(" Passou! " + resultado + " é maior ou igual a 60!");
+ }
 }
 
 
