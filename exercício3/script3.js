e1246228ebf13fca6a8c08481f742e0a40bcd988
raquel <raquelramoscampos285@gmail.com>
2025-05-27 08:43:48 -0300

    Add files via upload

---
+++ exercício3/script3.js
@@ -0,0 +1,16 @@
+function somar(){
+    let nota1 =  Number(window.prompt("Digite aqui sua primeira nota:"));
+    let nota2 =  Number(window.prompt("Digite aqui sua segunda nota:"));
+    let nota3 =  Number(window.prompt("Digite aqui sua terceira nota:"));
+
+    let resultado = nota1 + nota2 + nota3 /3
+    
+   if (resultado => 180){
+        alert(alert("Você passou!" + resultado + "."););      
+   } else {
+        (if resultado < 180) 
+            alert(Você não passou!) }
+
+}
+
+
